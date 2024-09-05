1b8705972d8ddccf9a2e6c15f83d17a6c2dec581
Nihil02 <samuelalva02@gmail.com>
2024-09-05 00:38:41 -0600

    Login frontend

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,29 @@
 <script>
-
 </script>
 
 <main>
-    
+	<div class="align-center align-middle">
+		<div class="grid h-screen place-items-center">
+			<div class="card rounded-lg min-h-72 min-w-96 p-4 shadow-md bg-tertiary-50">
+				<div class="grid h-full place-items-center">
+					<p>Usuario</p>
+					<input type="text" placeholder="Usuario" class="mt-4 rounded-container-token" />
+
+					<br />
+
+					<p>Contraseña</p>
+					<input type="password" placeholder="Contraseña" class="mt-4 rounded-container-token" />
+
+					<br />
+
+					<button
+						class="button_primary p-4"
+						on:click={() => {
+							console.log('Algo');
+						}}>Iniciar Sesión</button
+					>
+				</div>
+			</div>
+		</div>
+	</div>
 </main>
